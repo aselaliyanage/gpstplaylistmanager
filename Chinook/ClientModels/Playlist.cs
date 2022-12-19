dc92581e488129d18c563ff9f5020e4d20b95ff0
Asela Liyanage <aselal@99x.io>
2022-12-19 11:50:52 +0530

    Initial commit with changes

---
+++ Chinook/ClientModels/Playlist.cs
@@ -0,0 +1,8 @@
+namespace Chinook.ClientModels;
+
+public class Playlist
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public List<PlaylistTrack> Tracks { get; set; }
+}
